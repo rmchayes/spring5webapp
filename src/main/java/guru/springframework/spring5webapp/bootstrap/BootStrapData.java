c985d9f8b7f7c33ccf0665583628a8213c2e6dcf
rmch <rmch_work@icloud.com>
2020-12-14 16:13:18 -0700

    add pojos, repositories and boostrap

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
